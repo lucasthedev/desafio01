bda1797e38dc76bfe27e19aaf6d0ebd09326777d
Lucas P <lucas_wuthang@hotmail.com>
2020-01-29 22:54:13 -0300

    rotas implementadas

--- index.js
+++ index.js
@@ -3,8 +3,52 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/projetos', (req, resp) =>{
-    return resp.json({nome: 'Lucas'});
+const projetos = [{}];
+
+app.post('/projects', (req, resp)=>{
+    const {id} = req.body;
+    const {title} = req.body;
+
+    let obj = {
+        "id": id,
+        "title": title,
+        "tasks": []
+    }
+
+    projetos.push(obj);
+
+    return resp.json(projetos);
+
+});
+
+app.get('/projects', (req, resp) =>{
+    return resp.json(projetos);
+});
+
+app.put('/projects/:id', (req, resp) =>{
+    const {title} = req.body;
+    const {id} = req.params;
+
+    for(let i = 0; i < projetos.length; i++){
+        if(projetos[i].id == id){
+            projetos[i].title = title;
+        }
+    }
+
+    return resp.json(projetos);
+
+});
+
+app.delete('/projects/:id', (req, resp) => {
+    const {id} = req.params;
+
+    for(let i = 0; i < projetos.length; i++){
+        if(projetos[i].id == id){
+            projetos.splice(i, 1);
+        }
+    }
+
+    return resp.json(projetos);
 });
 
 app.listen(3001);
